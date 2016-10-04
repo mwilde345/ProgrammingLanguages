5bc24814c62ccec0596efe6c8cc2cb961f3d98cb
Matthew Wilde <mwilde345@gmail.com>
2016-10-04 07:18:19 -0600

    finished python

--- Python/ProblemSet.py
+++ Python/ProblemSet.py
@@ -1,7 +1,8 @@
 import math
+import operator
 
 def hypot(b, h):
-    return (b*h)/2
+    return math.sqrt(b**2+h**2)
 
 def radius(r):
     return ((4*math.pi)/3)*r**3
@@ -19,52 +20,76 @@ def dotprod(s1,s2):
     return dotProduct
 def duplicate(s1):
     return [x+" "+x for x in s1]
-def double(s1):
+def doubleIt(s1):
     return list(map(lambda x: x*2,s1))
 def noduplicate(s1):
     return set(s1)
-translator = {"99":"Neunundneunzig","red":"rot","balloons":"Luftballon"}
+translator = {"99":"Neunundneunzig","balloons":"Luftballon","red":"rot"}
 def translation(s1):
     return map(lambda x: translator[x],s1)
-
-#SUMPRODUCT
-def sumprod(s1,func):
-    return map(func,s1)
-def summify(list1):
-    intlist = [int(x) for x in list1]
-    return sum(intlist)
-def productify(list1):
-    return reduce(lambda x, y: int(x)*int(y), list1)
-#######################
-sumProductNums = raw_input("SumProduct Numbers: ").split(" ")
-print(sumprod(sumProductNums,summify))
-print(sumprod(sumProductNums,productify))
-"""
-translateString = raw_input("String to translate: ").split(" ")
-print(translation(translateString))
-
-duplicate1 = raw_input("Remove Duplicate List: ").split(" ")
-print(noduplicate(duplicate1))
-
-double1 = raw_input("Doubles/duplicate List: ").split(" ")
-print(duplicate(double1))
-print(double([int(x) for x in double1]))
-
-list1 = raw_input("Dot Product List 1: ")
-list2 = raw_input("Dot Product List 2: ")
-print(dotprod(list(map(int,list1.split(" "))),list(map(int,list2.split(" ")))))
+def sumprod(s1,oper):
+    return reduce(lambda x,y: oper(x,y), s1)
+def articles(s1):
+    return filter(lambda s: s.lower() in ['a','an','the'],s1)
+def colors(s1):
+    return filter(lambda s: s.lower() in list(('black','brown', 'blue', 'red', 'yellow', 'orange', 'purple', 'green', 'gray', 'pink')),s1)
+def positives(s1):
+    return filter(lambda s: s>0,s1)
+def getmax(s1):
+    return max(s1)
+def squareNums(x):
+    return filter(lambda x: x!=0,[x**2 for x in range(x+1)])
+def getYoungest(personList):
+    compare = lambda a,b: a[0] if (a[1]<b[1]) else b[0]
+    return reduce(compare,personList)
 
 def tester():
-b = input("Base for Hypotenuse: ")
-h = input("Height for Hypotenuse: ")
-print(hypot(b,h))
+    #colorList = raw_input("Color List: ").split(" ")
+    #print(colors(colorList))
+    #positiveList = [float(x) for x in raw_input("Pos List: ").split(" ")]
+    #print(positives(positiveList))
+    #maxList = [float(x) for x in raw_input("Max List: ").split(" ")]
+    #print(getmax(maxList))
+    #rangeNum = input("Square Number: ")
+    #print(squareNums(rangeNum))
+    """ bob = dict([('name',"Bob"),('age',27)])
+        bob2 = dict([('name',"Bob's Brother"),('age',26)])
+        bob3 = dict([('name',"Bob Jr"),('age',3)])
 
-r = input("Sphere Radius: ")
-print(radius(r))
+        b = input("Base for Hypotenuse: ")
+        h = input("Height for Hypotenuse: ")
+        print(hypot(b,h))
 
-t = input("Water Temperature ")
-print(water(t))
-"""
+        r = input("Sphere Radius: ")
+        print(radius(r))
 
-    #return "all finished"
-#print(tester())
+        t = input("Water Temperature ")
+        print(water(t))
+
+        sumProductNums = [int(x) for x in raw_input("SumProduct Numbers: ").split(" ")]
+        print(sumprod(sumProductNums,operator.add))
+        print(sumprod(sumProductNums,operator.mul))
+
+        translateString = raw_input("String to translate: ").split(" ")
+        print(translation(translateString))
+
+        duplicate1 = raw_input("Remove Duplicate List: ").split(" ")
+        print(noduplicate(duplicate1))
+
+        double1 = raw_input("Doubles/duplicate List: ").split(" ")
+        print(duplicate(double1))
+        print(doubleIt([int(x) for x in double1]))
+        list1 = raw_input("Dot Product List 1: ")
+        list2 = raw_input("Dot Product List 2: ")
+        print(dotprod(list(map(int,list1.split(" "))),list(map(int,list2.split(" ")))))
+
+        articleString = raw_input("Article String: ").split(" ")
+        print(articles(articleString))
+    """
+    b=("Bob",27)
+    b2=("Bob's Bro",26)
+    b3=("Bob Jr.",3)
+    print(getYoungest([b,b2,b3]))
+    return "all finished"
+
+print(tester())

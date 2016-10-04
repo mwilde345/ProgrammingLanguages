40e2afc99d4314e7a2900a84fefd061a1f0c15c0
Matthew Wilde <mwilde345@gmail.com>
2016-10-04 09:48:20 -0600

    tried nestlevel

--- Python/ProblemSet.py
+++ Python/ProblemSet.py
@@ -30,7 +30,7 @@ def translation(s1):
 def sumprod(s1,oper):
     return reduce(lambda x,y: oper(x,y), s1)
 def articles(s1):
-    return filter(lambda s: s.lower() in ['a','an','the'],s1)
+    return len(filter(lambda s: s.lower() in ['a','an','the'],s1))
 def colors(s1):
     return filter(lambda s: s.lower() in list(('black','brown', 'blue', 'red', 'yellow', 'orange', 'purple', 'green', 'gray', 'pink')),s1)
 def positives(s1):
@@ -42,54 +42,73 @@ def squareNums(x):
 def getYoungest(personList):
     compare = lambda a,b: a[0] if (a[1]<b[1]) else b[0]
     return reduce(compare,personList)
+def nested(nestList,level):
+    print(nestList[level])
+    if isinstance(nestList[level-1],list):
+        nested(filter(lambda l: isinstance(l,list),nestList),level+1)
+    else:
+        nested(nestList,level-1)
 
-def tester():
-    #colorList = raw_input("Color List: ").split(" ")
-    #print(colors(colorList))
-    #positiveList = [float(x) for x in raw_input("Pos List: ").split(" ")]
-    #print(positives(positiveList))
-    #maxList = [float(x) for x in raw_input("Max List: ").split(" ")]
-    #print(getmax(maxList))
-    #rangeNum = input("Square Number: ")
-    #print(squareNums(rangeNum))
-    """ bob = dict([('name',"Bob"),('age',27)])
-        bob2 = dict([('name',"Bob's Brother"),('age',26)])
-        bob3 = dict([('name',"Bob Jr"),('age',3)])
-
+def tester(response):
+    print(response)
+    if response=="color":
+        colorList = raw_input("Color List: ").split(" ")
+        print(colors(colorList))
+    elif response=="positive":
+        positiveList = [float(x) for x in raw_input("Pos List: ").split(" ")]
+        print(positives(positiveList))
+    elif response=="max":
+        maxList = [float(x) for x in raw_input("Max List: ").split(" ")]
+        print(getmax(maxList))
+    elif response=="square":
+        rangeNum = input("Square Number: ")
+        print(squareNums(rangeNum))
+    elif response=="people":
+        b=("Bob",27)
+        b2=("Bob's Bro",26)
+        b3=("Bob Jr.",3)
+        print(getYoungest([b,b2,b3]))
+    elif response=="hypotenuse":
         b = input("Base for Hypotenuse: ")
         h = input("Height for Hypotenuse: ")
         print(hypot(b,h))
-
+    elif response=="sphere":
         r = input("Sphere Radius: ")
         print(radius(r))
-
+    elif response=="water":
         t = input("Water Temperature ")
         print(water(t))
-
+    elif response=="sumproduct":
         sumProductNums = [int(x) for x in raw_input("SumProduct Numbers: ").split(" ")]
         print(sumprod(sumProductNums,operator.add))
         print(sumprod(sumProductNums,operator.mul))
-
+    elif response=="translate":
         translateString = raw_input("String to translate: ").split(" ")
         print(translation(translateString))
-
+    elif response=="nodupl":
         duplicate1 = raw_input("Remove Duplicate List: ").split(" ")
         print(noduplicate(duplicate1))
-
+    elif response=="double":
         double1 = raw_input("Doubles/duplicate List: ").split(" ")
         print(duplicate(double1))
         print(doubleIt([int(x) for x in double1]))
+    elif response=="dotprod":
         list1 = raw_input("Dot Product List 1: ")
         list2 = raw_input("Dot Product List 2: ")
         print(dotprod(list(map(int,list1.split(" "))),list(map(int,list2.split(" ")))))
-
+    elif response=="article":
         articleString = raw_input("Article String: ").split(" ")
         print(articles(articleString))
-    """
-    b=("Bob",27)
-    b2=("Bob's Bro",26)
-    b3=("Bob Jr.",3)
-    print(getYoungest([b,b2,b3]))
-    return "all finished"
-
-print(tester())
+    elif response=="nested":
+        nestList = [1,1,[2,[3,3]],1,[2,[3,[4,[5,5]],3]],1]
+        print("Running Nested On: "+str(nestList))
+        print(nested(nestList,1))
+    else:
+        print("No such method")
+def main():
+    methodRun = raw_input("Method to Run: ")
+    while methodRun!="quit":
+        tester(methodRun)
+        methodRun = raw_input("Next Method: ")
+    print("Good-Bye")
+main()
